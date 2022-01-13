6daffe7ec26040c7887913691c43520ee937d848
Michael Hranek <mhranek@comcast.net>
2022-01-12 19:23:56 -0500

    Any path now displays index.html 'home' page

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
 
 const PORT = process.env.port || 3001;
@@ -12,5 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // TODO Routing
+
+// GET /notes => notes.html
+
+// GET /api/notes => db.json
+
+// POST /api/notes => save db.json
+
+// BONUS DELETE /api/notes/:id => delete note with id
+
+// GET * => index.html
+app.get('*', (req, res) => res.sendfile(path.join(__dirname, '/public/index.html')))
 
 app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`))
